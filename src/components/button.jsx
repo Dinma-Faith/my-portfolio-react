ec0b9fb674ae716977064c733e7aadb9bb276f25
Dinma Faith <phaytep@gmail.com>
2025-09-12 22:51:10 +0100

    Create reusable Button component for links and actions

---
+++ src/components/button.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import styles from "../styles/button.module.css";
+
+const Button = ({ children, to, href, type = "button", onClick }) => {
+  // Button for Internal route
+  if (to) {
+    return (
+      <Link to={to} className={styles.btn}>
+        {children}
+      </Link>
+    );
+  }
+
+  //Button for External link
+  if (href) {
+    return (
+      <a href={href} target="_blank" rel="noreferrer" className={styles.btn}>
+        {children}
+      </a>
+    );
+  }
+
+  // Regular button for form submit or action
+  return (
+    <button type={type} onClick={onClick} className={styles.btn}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
